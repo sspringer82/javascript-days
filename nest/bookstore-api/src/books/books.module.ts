ed7b3a9fa128da4e902aa293af6b48bc45554a54
Sebastian Springer <Sebastian.Springer@rohde-schwarz.com>
2026-03-16 16:35:50 +0100

    prepare comments

--- nest/bookstore-api/src/books/books.module.ts
+++ nest/bookstore-api/src/books/books.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CommentsController } from './comments/comments.controller';
+import { CommentsService } from './comments/comments.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [BooksController],
-  providers: [BooksService],
+  controllers: [BooksController, CommentsController],
+  providers: [BooksService, CommentsService],
 })
 export class BooksModule {}
